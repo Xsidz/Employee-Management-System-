033425a39657188dc10cc384b3141be9e9fca60d
Xsidz <siddhesh.kabraa@gmail.com>
2025-07-21 23:15:09 +0530

    auth-employee

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState,useEffect } from "react";
+import { useState,useEffect, useContext } from "react";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard.jsx";
 import Login from "./components/Auth/Login";
 import {setLocalStorage} from "./utils/localStorage.jsx"
+import { AuthContext } from "./context/AuthProvider.jsx";
 
 const App = () => {
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    setLocalStorage()
-  }, [])
+  const authData = useContext(AuthContext)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
+  
+  // useEffect(() => {
+    
+  // if(authData){
+  //   const loggedInUser = localStorage.getItem("loggedInUser");
+  //   if(loggedInUser){
+  //     setUser(loggedInUser.role)
+  //   }
+  // }
+  //   return () => {
+      
+  //   }
+  // }, [authData])
+  
+
   
 
   const handelLogin = (email, password) => {
-    if (email == "admin@123.com" && password == 123) {
+    if (authData && authData.admin.find((e)=> email == e.email && password == e.password)) {
       setUser("admin");
-    } else if (email == "user@123.com" && password == 123) {
-      setUser("employee");
+      localStorage.setItem("loggedInUser",JSON.stringify({role:'admin'}))
+      const user = authData.admin.find((e)=>email == e.email)
+      console.log(user.id)
+     
+    } else if (authData ) {
+      const employee = authData.employees.find((e)=> email == e.email && password == e.password)
+      if(employee){
+        setUser("employee");
+        setLoggedInUserData(employee)
+      }
+      localStorage.setItem("loggedInUser",JSON.stringify({role:'employee'}))
     } else {
       console.log("Invalid Credentials");
     }
   };
   
+  
+
+
   return (
     <>
       {user == "admin" ? (
         <AdminDashboard />
       ) : user == "employee" ? (
-        <EmployeeDashboard />
+        <EmployeeDashboard data = {loggedInUserData} />
       ) : (
         <Login handelLogin={handelLogin} />
       )}
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,10 +3,12 @@ import Header from '../other/Header'
 import TaskListCount from '../other/TaskListCount'
 import TaskList from '../../TaskList/TaskList'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({data}) => {
+
   return (
     <>
     <div className='p-10 bg-[#1C1C1C1C]'>
+      
       <Header/>
       <TaskListCount/>
       <TaskList/>
